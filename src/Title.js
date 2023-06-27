0ea88df959379d99d7e079a104e99036603119b6
KellyMark <kmark602@gmail.com>
2023-06-27 17:46:57 -0400

    initial setup, add title

---
+++ src/Title.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Title() {
+    return (
+        <div>
+            <div id="title">
+                <h1>ZIGGY'S GARDEN PLANNER</h1>
+            </div>
+            <br></br><br></br>
+            <div id="subtitle">
+                <h4>A Phase 3 Project by Kelly Mark</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Title;
